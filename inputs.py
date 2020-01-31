a3aecf03844c47c54c2290ed1a0c74da97205192
Ken Nguyen <nguyen.ken@zoho.com>
2020-01-30 21:05:06 -0500

    Initial commit

---
+++ inputs.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+import re
+
+
+class UserInputs:
+    """Requests inputs from user for search query"""
+
+    def __init__(self):
+        """
+        states: list of all US states abbreviations
+        jobs: list of job titles
+        locations: dict of each state mapped to its cities
+        exp: specified experience level, None will include all jobs
+        """
+        self.states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+                       "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+                       "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+                       "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+                       "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+        self.jobs = []
+        self.locations = {}
+        self.exp = None
+
+    def get_jobs(self):
+        return self.jobs
+
+    def get_locations(self):
+        return self.locations
+    
+    def get_exp(self):
+        return self.exp
+
+    @staticmethod
+    def _clean_input(value):
+        """Removes non-alpha symbols from input except space"""
+        regex = re.compile("[^a-zA-Z ]")
+        new_value = regex.sub("", value)
+        return new_value
+
+    @staticmethod
+    def _process_string(value):
+        """Replace space with + for Indeed query"""
+        if " " in value:
+            value = value.strip().replace(" ", "+")
+        return value
+
+    def input_titles(self):
+        """Get user inputs for job titles and append to jobs list"""
+        titles = input("Enter all job titles: ")
+        
+        if not titles:
+            return
+
+        # One title
+        if "," not in titles:
+            titles = self._clean_input(titles)
+            titles = self._process_string(titles)
+            self.jobs.append(titles)
+            
+        # Multiple titles
+        else:
+            titles = titles.split(",")
+            for title in titles:
+                title = self._clean_input(title)
+                title = self._process_string(title)
+                self.jobs.append(title)
+
+    def input_states(self):
+        """Get user inputs for all states"""
+        states = input("Enter all states: ")
+
+        if not states:
+            print("Please enter the states you want to search in.")
+            return
+
+        # One state
+        if "," not in states:
+            states = states.upper()
+            try:
+                assert states in self.states, f"{states} is not a valid state."
+            except AssertionError:
+                return
+            states = self._clean_input(states)
+            self.locations[states] = []
+
+        # Multiple states
+        else:
+            states = states.split(",")
+            for state in states:
+                state = self._clean_input(state)
+                state = state.strip().upper()
+                try:
+                    assert state in self.states, f"{state} is not a valid state."
+                except AssertionError:
+                    return
+                self.locations[state] = []
+            
+    def input_cities(self):
+        """Get user inputs for all cities in each state"""
+        for state in self.locations:
+            cities = input(f"Enter all cities in {state}: ")
+
+            if not cities:
+                print("Please enter the cities of the states you want to search in.")
+                return
+
+            # One city
+            if "," not in cities:
+                cities = self._clean_input(cities)
+                cities = cities.title()
+                cities = self._process_string(cities)
+
+                self.locations[state].append(cities)
+
+            # Multiple cities
+            else:
+                cities = cities.split(",")
+                for city in cities:
+                    city = self._clean_input(city)
+                    city = city.strip().title()
+                    city = self._process_string(city)
+                    self.locations[state].append(city)
+
+    def input_exp(self):
+        """Get user input for desired experience level"""
+        exp = input("Enter experience level (entry/mid/senior): ")
+        
+        if not exp:
+            return
+
+        self.exp = self._clean_input(exp) + "_level"
---
+++ tests/test_inputs.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+from unittest.mock import patch
+from unittest import TestCase
+from inputs import UserInputs
+
+
+class TestInputTitles(TestCase):
+    
+    def setUp(self):
+        self.inputs = UserInputs()
+
+    def tearDown(self):
+        del self.inputs
+
+    @patch("builtins.input", return_value="")
+    def test_no_title(self, mock_input):
+        self.inputs.input_titles()
+        test = []
+        result = self.inputs.get_jobs()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="developer")
+    def test_one_word_title(self, mock_input):
+        self.inputs.input_titles()
+        test = ["developer"]
+        result = self.inputs.get_jobs()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="junior developer")
+    def test_multiple_words_title(self, mock_input):
+        self.inputs.input_titles()
+        test = ["junior+developer"]
+        result = self.inputs.get_jobs()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="developer, programmer, engineer")
+    def test_multiple_title(self, mock_input):
+        self.inputs.input_titles()
+        test = ["developer", "programmer", "engineer"]
+        result = self.inputs.get_jobs()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="java developer, java engineer")
+    def test_multipe_words_and_titles(self, mock_input):
+        self.inputs.input_titles()
+        test = ["java+developer", "java+engineer"]
+        result = self.inputs.get_jobs()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="!#^%@%eng@%i%ne(_)(er")
+    def test_invalid_title(self, mock_input):
+        self.inputs.input_titles()
+        test = ["engineer"]
+        result = self.inputs.get_jobs()
+        self.assertEqual(test, result)
+
+
+class TestInputStates(TestCase):
+
+    def setUp(self):
+        self.inputs = UserInputs()
+
+    def tearDown(self):
+        del self.inputs
+
+    @patch("builtins.input", return_value="")
+    def test_no_state(self, mock_input):
+        self.inputs.input_states()
+        test = {}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="ga")
+    def test_one_state(self, mock_input):
+        self.inputs.input_states()
+        test = {"GA": []}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="il, tx, pa")
+    def test_multiple_states(self, mock_input):
+        self.inputs.input_states()
+        test = {"IL": [], "TX": [], "PA": []}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="n%$!z, v(&n,   !^%c%!ad")
+    def test_invalid_states(self, mock_input):
+        self.inputs.input_states()
+        test = {}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+
+class TestInputCities(TestCase):
+
+    def setUp(self):
+        self.inputs = UserInputs()
+
+    def tearDown(self):
+        del self.inputs
+
+    @patch("builtins.input", side_effect=["al", ""])
+    def test_no_cities(self, mock_input):
+        self.inputs.input_states()
+        self.inputs.input_cities()
+        test = {"AL": []}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", side_effect=["tx", "dallas"])
+    def test_one_city_one_state(self, mock_input):
+        self.inputs.input_states()
+        self.inputs.input_cities()
+        test = {"TX": ["Dallas"]}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", side_effect=["ca", "los angeles, san francisco"])
+    def test_multiple_city_one_state(self, mock_input):
+        self.inputs.input_states()
+        self.inputs.input_cities()
+        test = {"CA": ["Los+Angeles", "San+Francisco"]}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", 
+           side_effect=["ga, nm, ny",
+                        "atlanta",
+                        "santa fe",
+                        "new york city"])
+    def test_one_city_multiple_states(self, mock_input):
+        self.inputs.input_states()
+        self.inputs.input_cities()
+        test = {'GA': ['Atlanta'], 
+                'NM': ['Santa+Fe'], 
+                'NY': ['New+York+City']}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", 
+           side_effect=["va, fl, il", 
+                        "richmond, virginia beach, alexandria", 
+                        "miami, fort lauderdale", 
+                        "chicago, rockford, arlington heights"])
+    def test_multiples_cities_and_states(self, mock_input):
+        self.inputs.input_states()
+        self.inputs.input_cities()
+        test = {"FL": ["Miami", "Fort+Lauderdale"],
+                "IL": ["Chicago", "Rockford", "Arlington+Heights"],
+                "VA": ["Richmond", "Virginia+Beach", "Alexandria"]}
+        result = self.inputs.get_locations()
+        self.assertEqual(test, result)
+
+
+class TestInputExp(TestCase):
+
+    def setUp(self):
+        self.inputs = UserInputs()
+
+    def tearDown(self):
+        del self.inputs
+
+    @patch("builtins.input", return_value="")
+    def test_no_exp(self, mock_input):
+        self.inputs.input_exp()
+        test = None
+        result = self.inputs.get_exp()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="entry")
+    def test_entry_exp(self, mock_input):
+        self.inputs.input_exp()
+        test = "entry_level"
+        result = self.inputs.get_exp()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="mid")
+    def test_mid_exp(self, mock_input):
+        self.inputs.input_exp()
+        test = "mid_level"
+        result = self.inputs.get_exp()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="senior")
+    def test_senior_exp(self, mock_input):
+        self.inputs.input_exp()
+        test = "senior_level"
+        result = self.inputs.get_exp()
+        self.assertEqual(test, result)
+
+    @patch("builtins.input", return_value="en!^$tr&^@#%$y")
+    def test_invalid_exp(self, mock_input):
+        self.inputs.input_exp()
+        test = "entry_level"
+        result = self.inputs.get_exp()
+        self.assertEqual(test, result)
