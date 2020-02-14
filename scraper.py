6d2d7d9d3de11d77502b937f640174591987453d
Ken Nguyen <30739815+kennguyen01@users.noreply.github.com>
2020-02-14 18:55:37 -0500

    Add additional fields to CSV header

--- scraper.py
+++ scraper.py
@@ -66,7 +66,7 @@ class IndeedScraper:
         Write all job postings to CSV file
         """
         with open("all-postings.csv", "a", encoding="utf-8") as csv_file:
-            header = ["Job Title", "Company", "Location", "Salary", "Link"]
+            header = ["Job Title", "Company", "Location", "Salary", "Link", "Applied", "Interviewed", "Offered", "Rejected"]
             writer = csv.writer(csv_file, dialect="excel")
             writer.writerow(header)
 
